cc264fffd51d9bc19fd7f8b832e358c4dbb93316
Ramon Bolds <ramonbolds98@gmail.com>
2022-03-27 23:47:30 -0500

    finished

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,14 +14,64 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-const people = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+// ● keyValue › returns an array with a sentence about each person with their name capitalized
+
+// ReferenceError: keyValue is not defined
+
+//   26 |   let result = ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+//   27 |   it("returns an array with a sentence about each person with their name capitalized", () => {
+// > 28 |     expect(keyValue(people)).toEqual(result)
+//      |     ^
+//   29 |   })
+//   30 | })
+//   31 |
+
+//   at Object.<anonymous> (code-challenges.test.js:28:5)
+
+// Test Suites: 1 failed, 1 total
 
 
+
+
+//1.) create function keyValue ()
+
+
+describe("keyValue", () => {
+  const people = [
+    { name: "ford prefect", occupation: "a hitchhiker" },
+    { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+    { name: "arthur dent", occupation: "a radio employee" }
+  ]
+  let result = ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+  it("returns an array with a sentence about each person with their name capitalized", () => {
+    expect(keyValue(people)).toEqual(result)
+  })
+})
+
+const keyValue = (obj)=>{
+    
+    let op = obj.map(({name, occupation}) => {
+      let str1 = `${name}`
+      let splitStr = str1.split(" ")
+       let newStr1 = splitStr.map(item=>item.charAt(0).toUpperCase()+ item.substring(1).toLowerCase());
+       let str2 = newStr1.join(" ")
+      let newStr = str2 + " is "+ `${occupation}`+"."
+      
+        return newStr
+    })
+    return op
+
+
+}
+// Test Suites: 1 passed, 1 total
+//1.Takes array of objects 
+//  3objects
+//all 3 have 2key/value pairs
+//2.Capitalizes first letter of both words in the value
+//3.convert the key/value into literals
+// add "is"between both literals
+// add "period" concat
+// 
 // b) Create the function that makes the test pass.
 
 
@@ -29,10 +79,40 @@ const people = [
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+// create a function remaindThree
+describe("remaindThree", () => {
+  const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+  // Expected output: [ 2, 0, -1, 0 ]
+  const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+  // Expected output: [ 2, 1, -1 ]
+  it("returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
+    expect(remaindThree(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(remaindThree(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
+//create a function takes array in mixed data
+// output:remainders of the number when divided by 3.
+//separate string & boolean values
+//Divide numbers byn 3 to
+//Return- Only Remainders/
+const remaindThree =(arr)=>{
+  let numerArr =arr.filter(value=>{
+    return typeof value === "number"
+  })
+  for(let i =0; i < numerArr.length; i++){
+    return value [i] %3
+  }
+  
+
+}
+//1.Create a function 
+//2. Separate string and boolean values
+//3. Divide numbers by 3
+//4. Return Only Remainders
+
+//Come back to can't figure out
+
+
 
 
 // b) Create the function that makes the test pass.
@@ -41,11 +121,38 @@ const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
+// creat function cubeSum function
+// ● cubeSum › returns the sum of all the numbers cubed
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
+//     ReferenceError: cubeSum is not defined
 
+//       119 |   // Expected output: 1125
+//       120 |   it("returns the sum of all the numbers cubed", () => {
+
+describe("cubeSum", () => {
+  const cubeAndSum1 = [2, 3, 4]
+  // Expected output: 99
+  const cubeAndSum2 = [0, 5, 10]
+  // Expected output: 1125
+  it("returns the sum of all the numbers cubed", () => {
+    expect(cubeSum(cubeAndSum1)).toEqual(99)
+    expect(cubeSum(cubeAndSum2)).toEqual(1125)
+  })
+})
+
+const cubeSum =(arr)=>{
+  //cube every number!
+ let newCube = arr.map(function(e){
+   return e*e*e
+ })
+ //Added the sum of the cubed numbers
+ let sum = newCube.reduce((partialSum,a)=> partialSum + a,0);
+ return sum
+}
+
+//1)creat a function takes array of numbers and returns
+//output sum of all numbers cubeAndSum2
+//1.Cube every number in array and
+//2. add the sum of the cubed numbers
 
 // b) Create the function that makes the test pass.
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1) Naming convention of controllers in Rails favors pluralizations of the last word in the controller's name, using this convention allows you to use default route generators without needing to qualify each :path or :controller, also it helps with consistency with named routes. A controller is a Ruby class which inherits from ApplicationController and has methods like other classes. Upon request, the routing  will determine necessary controller and action to run, then Rails creates an instance of that controller and runs the method with the same name as the action.
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) The @posts = BlogPost.all tells Rails to search the posts table and store each row it finds in the @posts instance object.
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3)The show method displays only further details on a single post. The show method's @post = BlogPost.find line tells Rails to find only the post that has the id defined in params [:id]
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4)The new method will let Rails know that you will create a new object. This method will be called when you display a page to the user to take user input.
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5)Once you have taken user input it is time to create a record into the database. The 1st line creates a new instance variable called @post, that holds a blog object built from the data, the user submitted. The blog_post params method is used to collect all the fields from object :blogpost. The data was passed from the new method to create using the params object. The next line is a conditional statement that redirect the user to the post method if object is valid. If object is not valid, the user is sent back to the new method. The redirect_to method is similar to performing a meta refreshon a web page, meaning a user is automatically sent there.
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -34,13 +34,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)
+    # ---6)Note:this edit method is similar to the show method. Both methods are used to retieve a single object  based on its id and and display it on the a page. The only difference is this is used to modify. This method finds or displays data that needs to be modified by the user.
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7)This method will be called after edit method, when the user modifies a data and wants to update changes into database. The update method is similar to create method, and will be used to update existing position database
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -54,15 +54,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)
+      # ---8)This second line redirect the user to @post method using a redirect_to call. 
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)
+  # ---9) Private method this is not accessible to outside of the class. A private method is defined by using a private keyword.
   private
   def blog_post_params
-    # ---10)
+    # ---10)The blog_post_params method is used to collect all fields from objec :post. The data was passed from the new method to create using the params object.
     params.require(:blog_post).permit(:title, :content)
   end
 end
